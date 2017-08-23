4d8c530e57c1edf0c97d443984d970d0a182e81c
garyemerson <gmohammadioun@gmail.com>
2017-08-23 07:44:04 -0700

    find closest snap point when dragging

--- Main.elm
+++ Main.elm
@@ -11,7 +11,7 @@ import Window
 import Task
 import Touch exposing (Coordinates)
 import SingleTouch
-import Puzzle
+import Puzzle exposing (Piece(..))
 import Dict exposing (Dict)
 import List exposing (map)
 import Tuple exposing (first, second)
@@ -27,12 +27,22 @@ main =
         }
 
 
+getOr : comparable -> v -> Dict comparable v -> v
+getOr key fallback dict =
+    case Dict.get key dict of
+        Nothing ->
+            fallback
+
+        Just val ->
+            val
+
+
 
 -- MODEL
 
 
 type alias Model =
-    { positions : Dict Int Position
+    { positions : Dict Int ( Puzzle.Piece, Position )
     , drag : Maybe Drag
     , winSize : WinSize
     , close : Bool
@@ -57,13 +67,108 @@ init =
     ( Model positionsInit Nothing (WinSize 100 100) False, Task.perform WinResize Window.size )
 
 
-positionsInit : Dict Int Position
+positionsInit : Dict Int ( Puzzle.Piece, Position )
 positionsInit =
     Dict.empty
-        |> Dict.insert 0 (Position 100 100)
-        |> Dict.insert 1 (Position 300 100)
-        |> Dict.insert 2 (Position 500 100)
-        |> Dict.insert 3 (Position 700 100)
+        |> Dict.insert 0 ( Center, (Position 100 100) )
+        |> Dict.insert 1 ( Center, (Position 100 250) )
+        |> Dict.insert 2 ( Center, (Position 100 400) )
+        |> Dict.insert 3 ( Center, (Position 250 400) )
+        |> Dict.insert 4 ( Center, (Position 400 400) )
+        |> Dict.insert 5 ( Center, (Position 400 250) )
+        |> Dict.insert 6 ( Center, (Position 400 100) )
+        |> Dict.insert 7 ( Center, (Position 250 100) )
+        |> Dict.insert 8 ( Center, (Position 250 250) )
+        |> Dict.insert 9 ( Center, (Position 600 100) )
+        |> Dict.insert 10 ( Center, (Position 750 100) )
+        |> Dict.insert 11 ( Center, (Position 600 250) )
+        |> Dict.insert 13 ( Center, (Position 600 400) )
+        |> Dict.insert 14 ( Center, (Position 600 400) )
+        |> Dict.insert 15 ( Center, (Position 600 400) )
+        |> Dict.insert 16 ( Center, (Position 600 400) )
+        |> Dict.insert 17 ( Center, (Position 600 400) )
+        |> Dict.insert 18 ( Center, (Position 600 400) )
+        |> Dict.insert 19 ( Center, (Position 600 400) )
+        |> Dict.insert 20 ( Center, (Position 600 400) )
+        |> Dict.insert 21 ( Center, (Position 600 400) )
+        |> Dict.insert 22 ( Center, (Position 600 400) )
+        |> Dict.insert 23 ( Center, (Position 600 400) )
+        |> Dict.insert 24 ( Center, (Position 600 400) )
+        |> Dict.insert 25 ( Center, (Position 600 400) )
+        |> Dict.insert 26 ( Center, (Position 600 400) )
+        |> Dict.insert 27 ( Center, (Position 600 400) )
+        |> Dict.insert 28 ( Center, (Position 600 400) )
+        |> Dict.insert 29 ( Center, (Position 600 400) )
+        |> Dict.insert 30 ( Center, (Position 600 400) )
+        |> Dict.insert 31 ( Center, (Position 600 400) )
+        |> Dict.insert 32 ( Center, (Position 600 400) )
+        |> Dict.insert 33 ( Center, (Position 600 400) )
+        |> Dict.insert 34 ( Center, (Position 600 400) )
+        |> Dict.insert 35 ( Center, (Position 600 400) )
+        |> Dict.insert 36 ( Center, (Position 600 400) )
+        |> Dict.insert 37 ( Center, (Position 600 400) )
+        |> Dict.insert 38 ( Center, (Position 600 400) )
+        |> Dict.insert 39 ( Center, (Position 600 400) )
+        |> Dict.insert 40 ( Center, (Position 600 400) )
+        |> Dict.insert 41 ( Center, (Position 600 400) )
+        |> Dict.insert 42 ( Center, (Position 600 400) )
+        |> Dict.insert 43 ( Center, (Position 600 400) )
+        |> Dict.insert 44 ( Center, (Position 600 400) )
+        |> Dict.insert 45 ( Center, (Position 600 400) )
+        |> Dict.insert 46 ( Center, (Position 600 400) )
+        |> Dict.insert 47 ( Center, (Position 600 400) )
+        |> Dict.insert 48 ( Center, (Position 600 400) )
+        |> Dict.insert 49 ( Center, (Position 600 400) )
+        |> Dict.insert 50 ( Center, (Position 600 400) )
+        |> Dict.insert 51 ( Center, (Position 600 400) )
+        |> Dict.insert 52 ( Center, (Position 600 400) )
+        |> Dict.insert 53 ( Center, (Position 600 400) )
+        |> Dict.insert 54 ( Center, (Position 600 400) )
+        |> Dict.insert 55 ( Center, (Position 600 400) )
+        |> Dict.insert 56 ( Center, (Position 600 400) )
+        |> Dict.insert 57 ( Center, (Position 600 400) )
+        |> Dict.insert 58 ( Center, (Position 600 400) )
+        |> Dict.insert 59 ( Center, (Position 600 400) )
+        |> Dict.insert 60 ( Center, (Position 600 400) )
+        |> Dict.insert 61 ( Center, (Position 600 400) )
+        |> Dict.insert 62 ( Center, (Position 600 400) )
+        |> Dict.insert 63 ( Center, (Position 600 400) )
+        |> Dict.insert 64 ( Center, (Position 600 400) )
+        |> Dict.insert 65 ( Center, (Position 600 400) )
+        |> Dict.insert 66 ( Center, (Position 600 400) )
+        |> Dict.insert 67 ( Center, (Position 600 400) )
+        |> Dict.insert 68 ( Center, (Position 600 400) )
+        |> Dict.insert 69 ( Center, (Position 600 400) )
+        |> Dict.insert 70 ( Center, (Position 600 400) )
+        |> Dict.insert 71 ( Center, (Position 600 400) )
+        |> Dict.insert 72 ( Center, (Position 600 400) )
+        |> Dict.insert 73 ( Center, (Position 600 400) )
+        |> Dict.insert 74 ( Center, (Position 600 400) )
+        |> Dict.insert 75 ( Center, (Position 600 400) )
+        |> Dict.insert 76 ( Center, (Position 600 400) )
+        |> Dict.insert 77 ( Center, (Position 600 400) )
+        |> Dict.insert 78 ( Center, (Position 600 400) )
+        |> Dict.insert 79 ( Center, (Position 600 400) )
+        |> Dict.insert 80 ( Center, (Position 600 400) )
+        |> Dict.insert 81 ( Center, (Position 600 400) )
+        |> Dict.insert 82 ( Center, (Position 600 400) )
+        |> Dict.insert 83 ( Center, (Position 600 400) )
+        |> Dict.insert 84 ( Center, (Position 600 400) )
+        |> Dict.insert 85 ( Center, (Position 600 400) )
+        |> Dict.insert 86 ( Center, (Position 600 400) )
+        |> Dict.insert 87 ( Center, (Position 600 400) )
+        |> Dict.insert 88 ( Center, (Position 600 400) )
+        |> Dict.insert 89 ( Center, (Position 600 400) )
+        |> Dict.insert 90 ( Center, (Position 600 400) )
+        |> Dict.insert 91 ( Center, (Position 600 400) )
+        |> Dict.insert 92 ( Center, (Position 600 400) )
+        |> Dict.insert 93 ( Center, (Position 600 400) )
+        |> Dict.insert 94 ( Center, (Position 600 400) )
+        |> Dict.insert 95 ( Center, (Position 600 400) )
+        |> Dict.insert 96 ( Center, (Position 600 400) )
+        |> Dict.insert 97 ( Center, (Position 600 400) )
+        |> Dict.insert 98 ( Center, (Position 600 400) )
+        |> Dict.insert 99 ( Center, (Position 600 400) )
 
 
 
@@ -77,8 +182,99 @@ type Msg
     | DragEnd Int Position
 
 
+closestSnapPoint : Int -> Model -> ( Float, Position, Puzzle.Knob )
+closestSnapPoint id model =
+    let
+        currKnobs =
+            let
+                ( piece, position ) =
+                    getPiecePosition id model
+            in
+                List.map (\( pt, knob ) -> ( pointToPosition pt, knob )) (Puzzle.knobPoints piece (positionToPoint position))
 
---| DragPuzzleStart Int Position
+        allOtherKnobs =
+            otherKnobs id model
+    in
+        case
+            (minThird
+                (List.foldr
+                    (::)
+                    []
+                    (List.map
+                        (\( pos, knob ) ->
+                            case knob of
+                                Puzzle.LeftKnob ->
+                                    closestSnapPointAux pos (List.filter (\( _, knob ) -> knob == Puzzle.RightKnob) allOtherKnobs)
+
+                                Puzzle.BottomKnob ->
+                                    closestSnapPointAux pos (List.filter (\( _, knob ) -> knob == Puzzle.TopKnob) allOtherKnobs)
+
+                                Puzzle.RightKnob ->
+                                    closestSnapPointAux pos (List.filter (\( _, knob ) -> knob == Puzzle.LeftKnob) allOtherKnobs)
+
+                                Puzzle.TopKnob ->
+                                    closestSnapPointAux pos (List.filter (\( _, knob ) -> knob == Puzzle.BottomKnob) allOtherKnobs)
+                        )
+                        currKnobs
+                    )
+                 --( 0, Position 0 0, Puzzle.LeftKnob )
+                )
+            )
+        of
+            Nothing ->
+                ( 0, Position 0 0, Puzzle.LeftKnob )
+
+            Just ( pos, knob, dist ) ->
+                ( dist, pos, knob )
+
+
+otherKnobs : Int -> Model -> List ( Position, Puzzle.Knob )
+otherKnobs id model =
+    List.foldr
+        (List.append)
+        []
+        (List.map (\( key, ( piece, pos ) ) -> List.map (\( pt, knob ) -> ( pointToPosition pt, knob )) (Puzzle.knobPoints piece (positionToPoint pos))) (List.filter (\( id2, _ ) -> id2 /= id) (Dict.toList model.positions)))
+
+
+closestSnapPointAux : Position -> List ( Position, Puzzle.Knob ) -> ( Position, Puzzle.Knob, Float )
+closestSnapPointAux position knobs =
+    case minThird (List.map (\( pos, knob ) -> ( pos, knob, dist pos position )) knobs) of
+        Nothing ->
+            ( Position 0 0, Puzzle.LeftKnob, 0 )
+
+        Just x ->
+            x
+
+
+{-| This returns the mininum tuple by comparing the third element. For example, the min
+in [(0, 1), (4, 0), (3, 2)] would be (4, 0).
+-}
+minThird : List ( a, b, comparable ) -> Maybe ( a, b, comparable )
+minThird list =
+    List.foldr
+        (\curr acc ->
+            case acc of
+                Nothing ->
+                    Just curr
+
+                Just accVal ->
+                    if (third curr) < (third accVal) then
+                        Just curr
+                    else
+                        Just accVal
+        )
+        Nothing
+        list
+
+
+third : ( a, b, c ) -> c
+third ( a, b, c ) =
+    c
+
+
+
+--(1, 2), (0, 4), (3, 7)
+--List.map (\()) (Dict.toList model.positions)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -91,12 +287,20 @@ update msg model =
             ( { model | drag = Just (Drag id position position) }, Cmd.none )
 
         DragAt id position ->
-            ( { model | drag = Maybe.map (\drag -> Drag drag.puzzleId drag.start position) model.drag, close = log "close" ((dist snapPoint (getPosition id model)) < toFloat snapRadius) }, Cmd.none )
+            ( { model
+                | drag =
+                    Maybe.map
+                        (\drag -> Drag drag.puzzleId drag.start position)
+                        model.drag
+                , close = (dist snapPoint (getPosition id model)) < toFloat snapRadius
+              }
+            , Cmd.none
+            )
 
         DragEnd id _ ->
             ( { model
                 | drag = Nothing
-                , positions = Dict.update id (always (Just (getPosition id model))) model.positions
+                , positions = Dict.update id (always (Just ( Center, (getPosition id model) ))) model.positions
 
                 --log "DragEnd"
                 --    (if model.close then
@@ -145,7 +349,7 @@ snapPoint =
 
 snapRadius : Int
 snapRadius =
-    20 + snapGuideRadius
+    20
 
 
 snapGuideRadius : Int
@@ -155,74 +359,36 @@ snapGuideRadius =
 
 view : Model -> Html Msg
 view model =
-    svg [ color "red", display "block", width (toString model.winSize.width), height (toString model.winSize.height) ]
-        (puzzlePieces model)
+    svg [ width (toString model.winSize.width), height (toString model.winSize.height) ]
+        (List.append
+            (puzzlePieces model)
+            (case model.drag of
+                Nothing ->
+                    []
 
-
-
---circle
---    [ onMouseDown
---    , SingleTouch.onStart (\coord -> DragStart (coordsToPosition (log "touch coord" coord)))
---    , SingleTouch.onMove (\coord -> DragAt (coordsToPosition (log "touch coord" coord)))
---    , SingleTouch.onEnd (\coord -> DragEnd (coordsToPosition (log "touch coord" coord)))
---    , Svg.Attributes.cursor "move"
---    , if model.close then
---        cx (toString snapPoint.x)
---      else
---        cx (toString position.x)
---    , if model.close then
---        cy (toString snapPoint.y)
---      else
---        cy (toString position.y)
---    , r "25%"
---    , fill "#0B79CE"
---    ]
---    []
---, circle
---    [ if model.close then
---        cx (toString snapPoint.x)
---      else
---        cx (toString position.x)
---    , if model.close then
---        cy (toString snapPoint.y)
---      else
---        cy (toString position.y)
---    , r (toString snapGuideRadius)
---    , fill "#000"
---    ]
---    []
---, Svg.text_
---    [ fontSize "22px"
---    , if model.close then
---        x (toString snapPoint.x)
---      else
---        x (toString position.x)
---    , if model.close then
---        y (toString (snapPoint.y + 100))
---      else
---        y (toString (position.y + 100))
---    ]
---    [ Svg.text "Drag to gray circle" ]
---, circle
---    [ cx (toString snapPoint.x)
---    , cy (toString snapPoint.y)
---    , r (toString (snapRadius - snapGuideRadius))
---    , fill "rgba(153, 153, 153, 0.75)"
---    ]
---    [] ,
---, Svg.text_
---    [ fontSize "22px"
---    , x "500"
---    , y "250"
---    ]
---    [ Svg.text (toString (Puzzle.mirror ( ( 10, 10 ), ( 16, -3 ), ( 16, -16 ) ))) ]
+                Just x ->
+                    let
+                        ( _, pos, _ ) =
+                            closestSnapPoint x.puzzleId model
+                    in
+                        [ circle
+                            [ cx (toString pos.x)
+                            , cy (toString pos.y)
+                            , r "3"
+                            , fill "yellow"
+                            ]
+                            []
+                        ]
+            )
+        )
 
 
 puzzlePieces : Model -> List (Svg Msg)
 puzzlePieces model =
     (map
         (\( key, val ) ->
-            Puzzle.center (positionToPoint (getPosition key model))
+            Puzzle.pieceSvg (first val)
+                (positionToPoint (getPosition key model))
                 [ onMouseDown (first ( key, val ))
                 , SingleTouch.onStart (\coord -> DragStart key (coordsToPosition coord))
                 , SingleTouch.onMove (\coord -> DragAt key (coordsToPosition coord))
@@ -238,6 +404,11 @@ coordsToPosition coords =
     Position (truncate coords.clientX) (truncate coords.clientY)
 
 
+pointToPosition : Puzzle.Point -> Position
+pointToPosition pt =
+    Position (truncate pt.x) (truncate pt.y)
+
+
 positionToPoint : Position -> Puzzle.Point
 positionToPoint position =
     Puzzle.Point (toFloat position.x) (toFloat position.y)
@@ -248,14 +419,22 @@ onMouseDown id =
     on "mousedown" (Decode.map (DragStart id) Mouse.position)
 
 
+{-| getPosition
+TODO: the dict lookup is unnecessary and should be removed
+-}
 getPosition : Int -> Model -> Position
-getPosition id { positions, drag, winSize } =
+getPosition id model =
+    second (getPiecePosition id model)
+
+
+getPiecePosition : Int -> Model -> ( Puzzle.Piece, Position )
+getPiecePosition id { positions, drag, winSize } =
     case drag of
         Nothing ->
             case Dict.get id positions of
                 Nothing ->
                     -- TODO: this should never happen, why does it need to be here?
-                    Position 0 0
+                    ( Puzzle.Center, Position 0 0 )
 
                 Just position ->
                     position
@@ -263,17 +442,20 @@ getPosition id { positions, drag, winSize } =
         Just { puzzleId, start, current } ->
             let
                 position =
-                    case Dict.get id positions of
+                    (case Dict.get id positions of
                         Nothing ->
                             -- TODO: this should never happen, why does it need to be here?
-                            Position 0 0
+                            ( Puzzle.Center, Position 0 0 )
 
                         Just position ->
                             position
+                    )
             in
                 if id == puzzleId then
-                    Position
-                        (position.x + current.x - start.x)
-                        (position.y + current.y - start.y)
+                    ( first position
+                    , Position
+                        ((second position).x + current.x - start.x)
+                        ((second position).y + current.y - start.y)
+                    )
                 else
-                    Position position.x position.y
+                    position
--- Puzzle.elm
+++ Puzzle.elm
@@ -5,28 +5,97 @@ import Svg.Attributes exposing (..)
 import List exposing (append)
 
 
-{-
-   The 13 different types of puzzle pieces are:
-   ┌───┐  ┌───┐  ┌───┐
-   │    > >    > >   │
-   └─^─┘  └─^─┘  └─^─┘                ┌───┐
-   ┌─^─┐  ┌─^─┐  ┌─^─┐  ┌───┐  ┌───┐  │   │
-   │    > >    > >   │  │    > >   │  └─^─┘
-   └─^─┘  └─^─┘  └─^─┘  └───┘  └───┘  ┌─^─┐
-   ┌─^─┐  ┌─^─┐  ┌─^─┐                │   │
-   │    > >   >  >   │                └───┘
-   └───┘  └───┘  └───┘
--}
+--Point -> List (Attribute msg) -> Svg msg
+
+
+type Piece
+    = Bottom
+    | Top
+    | Right
+    | Left
+    | MidLeft
+    | BottomLeft
+    | BottomMid
+    | BottomRight
+    | MidRight
+    | TopRight
+    | TopMid
+    | TopLeft
+    | Center
+
+
+type Knob
+    = LeftKnob
+    | BottomKnob
+    | RightKnob
+    | TopKnob
+
+
+
+--{ svg : List (Attribute msg) -> Svg msg
+--, leftKnobOffset : Maybe Point
+--, bottomKnobOffset : Maybe Point
+--, rightKnobOffset : Maybe Point
+--, topKnobOffset : Maybe Point
+--}
 
 
 type alias Point =
     { x : Float, y : Float }
 
 
-{-| The origin/move point of a puzzle piece is the top left corner
+addPts : Point -> Point -> Point
+addPts pt1 pt2 =
+    Point (pt1.x + pt2.x) (pt1.y + pt2.y)
+
+
+knobPoints : Piece -> Point -> List ( Point, Knob )
+knobPoints piece point =
+    case piece of
+        Bottom ->
+            [ ( addPts point topKnobOffset, TopKnob ) ]
+
+        Top ->
+            [ ( addPts point bottomKnobOffset, BottomKnob ) ]
+
+        Right ->
+            [ ( addPts point leftKnobOffset, LeftKnob ) ]
+
+        Left ->
+            [ ( addPts point rightKnobOffset, RightKnob ) ]
+
+        MidLeft ->
+            [ ( addPts point bottomKnobOffset, BottomKnob ), ( addPts point rightKnobOffset, RightKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+        BottomLeft ->
+            [ ( addPts point rightKnobOffset, RightKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+        BottomMid ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point rightKnobOffset, RightKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+        BottomRight ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+        MidRight ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point bottomKnobOffset, BottomKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+        TopRight ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point bottomKnobOffset, BottomKnob ) ]
+
+        TopMid ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point bottomKnobOffset, BottomKnob ), ( addPts point rightKnobOffset, RightKnob ) ]
+
+        TopLeft ->
+            [ ( addPts point bottomKnobOffset, BottomKnob ), ( addPts point rightKnobOffset, RightKnob ) ]
+
+        Center ->
+            [ ( addPts point leftKnobOffset, LeftKnob ), ( addPts point bottomKnobOffset, BottomKnob ), ( addPts point rightKnobOffset, RightKnob ), ( addPts point topKnobOffset, TopKnob ) ]
+
+
+{-| Auxilary fn to create svg for a puzzle piece given the specified sides.
 -}
-center : Point -> List (Attribute msg) -> Svg msg
-center topLeft attrs =
+pieceSvgAux : String -> String -> String -> String -> Point -> List (Attribute msg) -> Svg msg
+pieceSvgAux left bottom right top point attrs =
     svg
         (append
             attrs
@@ -42,13 +111,13 @@ center topLeft attrs =
                     " "
                     [ String.join " "
                         [ "M"
-                        , toString topLeft.x
-                        , toString topLeft.y
+                        , toString point.x
+                        , toString point.y
                         ]
-                    , leftKnob
-                    , bottomKnob
-                    , rightKnob
-                    , topKnob
+                    , left
+                    , bottom
+                    , right
+                    , top
                     ]
                 )
             ]
@@ -56,7 +125,132 @@ center topLeft attrs =
         ]
 
 
-{-| Auxliary fn to mirror 'c' path elements (bezier curves) about the x axis
+
+{-
+   The 13 different types of puzzle pieces are:
+    ┌───┐  ┌───┐  ┌───┐
+    │    > >    > >   │
+    └─^─┘  └─^─┘  └─^─┘                ┌───┐
+    ┌─^─┐  ┌─^─┐  ┌─^─┐  ┌───┐  ┌───┐  │   │
+    │    > >    > >   │  │    > >   │  └─^─┘
+    └─^─┘  └─^─┘  └─^─┘  └───┘  └───┘  ┌─^─┐
+    ┌─^─┐  ┌─^─┐  ┌─^─┐                │   │
+    │    > >   >  >   │                └───┘
+    └───┘  └───┘  └───┘
+
+   The origin/point/move point of a puzzle piece is the top left corner. The svg path runs counterclockwise.
+-}
+{- offsets from top left origin
+   left     (34, 59)
+   bottom   (59, 84)
+   right    (152, 59)
+   top      (59, -34)
+-}
+
+
+leftKnobOffset : Point
+leftKnobOffset =
+    Point 34 59
+
+
+bottomKnobOffset : Point
+bottomKnobOffset =
+    Point 59 84
+
+
+rightKnobOffset : Point
+rightKnobOffset =
+    Point 152 59
+
+
+topKnobOffset : Point
+topKnobOffset =
+    Point 59 -34
+
+
+pieceSvg : Piece -> Point -> List (Attribute msg) -> Svg msg
+pieceSvg piece point attrs =
+    case piece of
+        Bottom ->
+            --bottomSvg : Point -> List (Attribute msg) -> Svg msg
+            --bottomSvg point attrs =
+            pieceSvgAux leftLine bottomLine rightLine topKnob point attrs
+
+        --}
+        Top ->
+            --topSvg : Point -> List (Attribute msg) -> Svg msg
+            --topSvg point attrs =
+            pieceSvgAux leftLine bottomKnob rightLine topLine point attrs
+
+        --}
+        Right ->
+            --rightSvg : Point -> List (Attribute msg) -> Svg msg
+            --rightSvg point attrs =
+            pieceSvgAux leftKnob bottomLine rightLine topLine point attrs
+
+        --}
+        Left ->
+            --leftSvg : Point -> List (Attribute msg) -> Svg msg
+            --leftSvg point attrs =
+            pieceSvgAux leftLine bottomLine rightKnob topLine point attrs
+
+        --}
+        MidLeft ->
+            --midLeftSvg : Point -> List (Attribute msg) -> Svg msg
+            --midLeftSvg point attrs =
+            pieceSvgAux leftLine bottomKnob rightKnob topKnob point attrs
+
+        --}
+        BottomLeft ->
+            --bottomLeftSvg : Point -> List (Attribute msg) -> Svg msg
+            --bottomLeftSvg point attrs =
+            pieceSvgAux leftLine bottomLine rightKnob topKnob point attrs
+
+        --}
+        BottomMid ->
+            --bottomMidSvg : Point -> List (Attribute msg) -> Svg msg
+            --bottomMidSvg point attrs =
+            pieceSvgAux leftKnob bottomLine rightKnob topKnob point attrs
+
+        --}
+        BottomRight ->
+            --bottomRightSvg : Point -> List (Attribute msg) -> Svg msg
+            --bottomRightSvg point attrs =
+            pieceSvgAux leftKnob bottomLine rightLine topKnob point attrs
+
+        --}
+        MidRight ->
+            --midRightSvg : Point -> List (Attribute msg) -> Svg msg
+            --midRightSvg point attrs =
+            pieceSvgAux leftKnob bottomKnob rightLine topKnob point attrs
+
+        --}
+        TopRight ->
+            --topRightSvg : Point -> List (Attribute msg) -> Svg msg
+            --topRightSvg point attrs =
+            pieceSvgAux leftKnob bottomKnob rightLine topLine point attrs
+
+        --}
+        TopMid ->
+            --topMidSvg : Point -> List (Attribute msg) -> Svg msg
+            --topMidSvg point attrs =
+            pieceSvgAux leftKnob bottomKnob rightKnob topLine point attrs
+
+        --}
+        TopLeft ->
+            --topLeftSvg : Point -> List (Attribute msg) -> Svg msg
+            --topLeftSvg point attrs =
+            pieceSvgAux leftLine bottomKnob rightKnob topLine point attrs
+
+        --}
+        Center ->
+            --centerSvg : Point -> List (Attribute msg) -> Svg msg
+            --centerSvg point attrs =
+            pieceSvgAux leftKnob bottomKnob rightKnob topKnob point attrs
+--}
+
+
+{-| Auxiliary fn to mirror 'c' path elements (bezier curves) about the x axis
 -}
 mirror : ( ( Float, Float ), ( Float, Float ), ( Float, Float ) ) -> ( ( Float, Float ), ( Float, Float ), ( Float, Float ) )
 mirror ( ( x1, y1 ), ( x2, y2 ), ( x3, y3 ) ) =
@@ -65,6 +259,24 @@ mirror ( ( x1, y1 ), ( x2, y2 ), ( x3, y3 ) ) =
             ( -x3, y3 )
     in
         ( ( x2 - x3, -(y2 - y3) ), ( dx + x1, dy - y1 ), ( dx, dy ) )
+
+
+
+{-
+      (18 + 16, 35 + 8 + 16)
+      (34, 59)
+
+      * For rightKnob and leftKnob the center of the knob is (34, 59) away from its uppermost point
+      * For topKnob and bottomKnob the center of the know is (59, -34) away from its leftmost point
+
+      118 long
+
+   offsets from top left origin
+      left     (34, 59)
+      bottom   (59, 84)
+      right    (152, 59)
+      top      (59, -34)
+-}
 
 
 rightKnob : String
@@ -113,3 +325,23 @@ bottomKnob =
         , "c -10 10, -5 18, 8 18"
         , "l 35 0"
         ]
+
+
+rightLine : String
+rightLine =
+    "l 0 -91"
+
+
+topLine : String
+topLine =
+    "l -91 0"
+
+
+leftLine : String
+leftLine =
+    "l 0 91"
+
+
+bottomLine : String
+bottomLine =
+    "l 91 0"
